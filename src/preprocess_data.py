885865bbcb9b70495b7e9559ca40a8508f815f04
Preetham <preetham.ganesh2015@gmail.com>
2024-12-11 14:16:17 -0500

    PBMS-38 added extract_data_from_zip_file().
    Extracts files from downloaded data zip file.

---
+++ src/preprocess_data.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import warnings
+import argparse
+import zipfile
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_PATH)
+warnings.filterwarnings("ignore")
+
+
+from src.utils import check_directory_path_existence
+
+
+def extract_data_from_zip_file() -> None:
+    """Extracts files from downloaded data zip file.
+
+    Extracts files from downloaded data zip file.
+
+    Args:
+        None.
+
+    Returns:
+        None.
+    """
+    # Creates absolute directory path for downloaded data zip file.
+    home_directory_path = os.getcwd()
+    zip_file_path = "{}/data/raw_data/archive.zip".format(home_directory_path)
+
+    # Creates the directory path.
+    extracted_data_directory_path = check_directory_path_existence(
+        "data/extracted_data/lgg_mri_segmentation"
+    )
+
+    # If file does not exist, then extracts files from the directory.
+    if not os.path.exists(
+        "{}/kaggle_3m/data.csv".format(extracted_data_directory_path)
+    ):
+
+        # Extracts files from downloaded data zip file into a directory.
+        try:
+            with zipfile.ZipFile(zip_file_path, "r") as zip_file:
+                zip_file.extractall(extracted_data_directory_path)
+        except FileNotFoundError as error:
+            raise FileNotFoundError(
+                "{} does not exist. Download data from "
+                "'https://www.kaggle.com/datasets/mateuszbuda/lgg-mri-segmentation', and place it in "
+                "'data/raw_data' as 'archive.zip'.".format(zip_file_path)
+            )
+        print(
+            "Finished extracting files from 'archive.zip' to {}.".format(
+                extracted_data_directory_path
+            )
+        )
+        print()
