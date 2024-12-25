52de74d9bef8c0dc3ec04f13efad8b19e1e422ad
Preetham <preetham.ganesh2015@gmail.com>
2024-12-24 23:17:57 -0500

    [#5] updated check_directory_path_existence().
    updated folder concatenated using os.path.join.

--- src/utils.py
+++ src/utils.py
@@ -19,7 +19,7 @@ def check_directory_path_existence(directory_path: str) -> str:
 
     # Creates the following directory path if it does not exist.
     home_directory_path = os.getcwd()
-    absolute_directory_path = "{}/{}".format(home_directory_path, directory_path)
+    absolute_directory_path = os.path.join(home_directory_path, directory_path)
     if not os.path.isdir(absolute_directory_path):
         os.makedirs(absolute_directory_path)
     return absolute_directory_path
