852a9ef9bafe50cb0f840d2ec875295f5ffc224a
Preetham <preetham.ganesh2015@gmail.com>
2024-12-25 12:25:00 -0500

    [#6] added load_dataset() to Train class.
    Loads the dataset based on model configuration.

--- src/flair_abnormality_classification/train.py
+++ src/flair_abnormality_classification/train.py
@@ -1,6 +1,7 @@
 import os
 
 from src.utils import load_json_file
+from src.flair_abnormality_classification.dataset import Dataset
 
 
 class Train(object):
@@ -40,5 +41,28 @@ class Train(object):
             self.home_directory_path, "configs/flair_abnormality_classification"
         )
         self.model_configuration = load_json_file(
-            "v{}".format(self.model_version), model_configuration_directory_path
+            f"v{self.model_version}", model_configuration_directory_path
         )
+
+    def load_dataset(self) -> None:
+        """Loads the dataset based on model configuration.
+
+        Loads the dataset based on model configuration.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # Initializes object for the Dataset class.
+        self.dataset = Dataset(self.model_configuration)
+
+        # Loads the metadata for the images in the dataset.
+        self.dataset.load_images_metadata
+
+        # Splits file paths into new train, validation & test file paths.
+        self.dataset.split_dataset()
+
+        # Converts split data into tensor dataset & slices them based on batch size.
+        self.dataset.shuffle_slice_dataset()
