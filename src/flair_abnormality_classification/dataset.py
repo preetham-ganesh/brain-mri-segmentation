71c56700e8f316a3f2891681ed3fee37f5b587cb
Preetham <preetham.ganesh2015@gmail.com>
2024-12-25 11:23:01 -0500

    [#5] added load_images_metadata() to Dataset class.
    Loads the metadata for the images in the dataset.

--- src/flair_abnormality_classification/dataset.py
+++ src/flair_abnormality_classification/dataset.py
@@ -1,3 +1,7 @@
+import os
+
+from src.utils import check_directory_path_existence
+
 from typing import Dict, List, Any
 
 
@@ -22,3 +26,59 @@ class Dataset(object):
 
         # Initalizes class variables.
         self.model_configuration = model_configuration
+
+    def load_images_metadata(self) -> None:
+        """Loads the metadata for the images in the dataset.
+
+        Loads the metadata for the images in the dataset.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # Creates absolute directory paths for the following image directories.
+        base_directory_path = (
+            "data/processed_data/lgg_mri_segmentation/"
+            + f"v{self.model_configuration['dataset']['version']}"
+        )
+        images_directory_paths = {
+            "no_abnormality": check_directory_path_existence(
+                os.path.join(base_directory_path, "no_abnormality/images")
+            ),
+            "abnormality": check_directory_path_existence(
+                os.path.join(base_directory_path, "abnormality/images")
+            ),
+        }
+
+        # Iterates across directories in the dataset.
+        self.images_file_paths, self.labels = list(), list()
+        for label, directory_path in enumerate(images_directory_paths.values()):
+
+            # Lists names of files in the directory.
+            image_names = os.listdir(directory_path)
+
+            # Iterates across images in the directory.
+            for i_id in range(len(image_names)):
+
+                # If first letter of image name is '.', then ignores the image.
+                if image_names[i_id][0] == ".":
+                    continue
+
+                # Appends image file path & class to the list.
+                self.images_file_paths.append(
+                    os.path.join(directory_path, image_names[i_id])
+                )
+                self.labels.append(label)
+        print(
+            f"No. of valid images in the processed dataset: {len(self.images_file_paths)}"
+        )
+        print()
+        print(
+            f"No. of abnormality class images in the processed dataset: {self.labels.count(1)}"
+        )
+        print(
+            f"No. of no abnormality class images in the processed dataset: {self.labels.count(0)}"
+        )
+        print()
