f5c57cf5253a3409f82936845367c9b61be41a2f
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2024-12-27 17:16:18 -0500

    [#16] added save_model() to Train class.
    Saves the model after checking performance metrics in current epoch.

--- src/flair_abnormality_segmentation/train.py
+++ src/flair_abnormality_segmentation/train.py
@@ -430,3 +430,17 @@ class Train(object):
             step=epoch,
         )
         print("")
+
+    def save_model(self) -> None:
+        """Saves the model after checking performance metrics in current epoch.
+
+        Saves the model after checking performance metrics in current epoch.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.manager.save()
+        print(f"Checkpoint saved at {self.checkpoint_directory_path}.")
